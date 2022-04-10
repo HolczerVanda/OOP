cf75fa21696d8c1b85747ec1c24dabb7604b7073
Holczer Vanda <vanda.holczer@gmail.com>
2022-04-10 23:15:04 +0200

    fixed ebook

--- tax/EBook.java
+++ tax/EBook.java
@@ -1,5 +1,35 @@
 package tax;
 
-public class EBook {
+public class EBook extends Book {
 
+	private String url;
+	
+	public EBook(String title, String author, int price, int pages, int nettoPrice, String url) {
+		super(title, author, pages, nettoPrice);
+		this.url=url;
+	}
+	
+	
+	
+	
+	
+	/*@Override
+	public String toString()
+	{
+		return "Title: "+getTitle()+"\nAuthor: "+getAuthor()+"\nYear of publication: "+getYearOfPublication()+"\nPrice: "+getPrice()+" Ft\n"+"Page number: "+getPages()+"\nStyle: "+getStyle()+"\nURL: "+url;
+	}*/
+
+	@Override
+	public String toString() {
+		return "\nUrl: " + url + "\nTitle: " + getName() + "\nAuthor: " + getAuthor()+"\nPages: " + getPages() + "\nGross price: " + getTaxedValue() + "\n";
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
 }
